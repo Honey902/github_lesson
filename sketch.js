ce32433c6afb9a895d9f3809b28dd313a8bf3139
Honey902 ajcip <honeyajaj@icloud.com>
2024-04-17 16:59:04 +0000

    I copy and pasted this heart project

--- sketch.js
+++ sketch.js
@@ -1,40 +1,20 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  translate(width/2,height/2);
+  fill("pink")
+  stroke("brown")
+  strokeWeight(5)
+  beginShape();
   
-  function draw() {
-    background(220);
-    background("green")
-    strokeWeight(5)
-    fill('white')
-    ellipse(175, 300, 50, 30)
-    ellipse(225, 300, 50, 30)
-    fill('yellow')
-    ellipse(165, 250, 30, 50)
-    ellipse(235, 250, 30, 50)
-    rect(160, 210, 80, 80)
-    
-    
-    
-    triangle(165, 220, 200, 230, 185, 240)
-    triangle(235, 220, 200, 230, 215, 240)
-    fill('white')
-    strokeWeight(3)
-    ellipse(200, 242, 10, 10)
-    strokeWeight(5)
-    ellipse(170, 115, 45, 80)
-    ellipse(230, 115, 45, 80)
-    ellipse(200, 180, 120, 100)
-    strokeWeight(0)
-  
-    ellipse(175, 140, 35, 25)
-    ellipse(225, 140, 35, 25)
-    fill('black')
-    ellipse(180, 185, 10, 10)
-    ellipse(220, 185, 10, 10)
-    strokeWeight(5)
-    line(195, 200, 205, 205);
-    line(195, 205, 205, 200);
-    
-    
+  for (var a =0; a < TWO_PI; a+=0.01){
+    var r = 10
+    var x=r*16 * pow(sin(a), 3);
+    var y = -r* (13* cos(a) - 5 *cos(2*a) - 2* cos(3*a)- cos(4*a));
+    vertex(x,y)
   }
+  endShape()
+}
